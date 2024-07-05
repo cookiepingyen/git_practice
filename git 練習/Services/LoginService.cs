37be8638ec9593f630f676b63e8faa00c1c816a4
yen <yen@LAPTOP-JR0ECM7S>
2024-07-05 19:56:11 +0800

    新增 LoginService

---
+++ git 練習/Services/LoginService.cs
@@ -0,0 +1,17 @@
+﻿using git_練習.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace git_練習.Services
+{
+    public class LoginService
+    {
+        public bool Login(UserModel model)
+        {
+            return true;
+        }
+    }
+}
