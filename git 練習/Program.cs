88960219f6f3c5d5730b99e4b609cff1cf7b5282
yen <yen@LAPTOP-JR0ECM7S>
2024-07-05 20:32:26 +0800

    Change loginService position

--- git 練習/Program.cs
+++ git 練習/Program.cs
@@ -15,6 +15,15 @@ namespace git_練習
     {
         static void Main(string[] args)
         {
+            #region 登入模組
+            LoginService loginService = new LoginService();
+            UserModel user = new UserModel("yen", "aaa", "password", "taipei", "phone");
+            loginService.IfAlreadyLogin(user);
+            loginService.Login(user);
+            loginService.TwoStepValidation(user);
+            #endregion
+
+
             #region 註冊模組
             Console.WriteLine("歡迎使用者登入我們的GIT系統，請先輸入註冊訊息，以利後續登入做使用");
             while (true)
@@ -24,7 +33,7 @@ namespace git_練習
                 string account = Console.ReadLine();
                 string password = Console.ReadLine();
                 string phone = Console.ReadLine();
-                UserModel registerModel = new UserModel(name, email, account, password,phone);
+                UserModel registerModel = new UserModel(name, email, account, password, phone);
                 RegisterService service = new RegisterService();
                 bool isSuccess = service.Regist(registerModel);
 
@@ -42,13 +51,8 @@ namespace git_練習
 
             Console.ReadKey();
 
-            LoginService loginService = new LoginService();
 
-            UserModel user = new UserModel("yen", "aaa", "password", "taipei", "phone");
 
-            loginService.IfAlreadyLogin(user);
-            loginService.Login(user);
-            loginService.TwoStepValidation(user);
 
         }
 
