9f40b5c94d6e9bd5bc0555d270e7b01468ed0657
周政輝 <leo604leo604@gmail.com>
2024-07-05 19:45:37 +0800

    新增註冊函數

--- git 練習/Services/RegisterService.cs
+++ git 練習/Services/RegisterService.cs
@@ -1,7 +1,12 @@
+using git_練習.Models;
+
 namespace git_練習.Services
 {
     public class RegisterService
     {
-        
+        public bool Regist(UserModel model)
+        {
+            return true;
+        }
     }
 }
