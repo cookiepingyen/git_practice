9f0251daf1d2093c407422e09e638c35ff27f95f
周政輝 <leo604leo604@gmail.com>
2024-07-05 19:55:26 +0800

    完成會員公司註冊

--- git 練習/Services/RegisterService.cs
+++ git 練習/Services/RegisterService.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Runtime.ConstrainedExecution;
+using System.Xml;
 using git_練習.Models;
 
 namespace git_練習.Services
@@ -6,6 +9,16 @@ namespace git_練習.Services
     {
         public bool Regist(UserModel model)
         {
+            if (model.Email.Equals("leo604leo604@gmail.com"))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool SendEmail(string content)
+        {   
+            Console.WriteLine("發送驗證信");
             return true;
         }
     }
